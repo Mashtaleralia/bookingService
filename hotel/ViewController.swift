e470cdc69167f51874896f1ea9a38ca8390d1040
Alevtina Mashtaler <alya.mashtalez@gmail.com>
2023-08-31 16:47:56 +0300

    Initial Commit

---
+++ hotel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hotel
+//
+//  Created by Admin on 31.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
